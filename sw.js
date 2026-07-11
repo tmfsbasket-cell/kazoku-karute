acd99404556ffabaaadffeb4ed8dcd32bd3bfdab
tmfsbasket-cell <tm.f.s.basket@gmail.com>
2026-07-11 19:52:16 +0900

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,23 @@
+/* かぞくカルテ Service Worker
+   更新のたびに CACHE_NAME のバージョン番号を必ず変えること */
+var CACHE_NAME = 'kazoku-v0.2.1';
+var ASSETS = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png'];
+
+self.addEventListener('install', function (event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(function (c) { return c.addAll(ASSETS); })
+      .then(function () { return self.skipWaiting(); })
+  );
+});
+self.addEventListener('activate', function (event) {
+  event.waitUntil(
+    caches.keys().then(function (keys) {
+      return Promise.all(keys.map(function (k) { if (k !== CACHE_NAME) return caches.delete(k); }));
+    }).then(function () { return self.clients.claim(); })
+  );
+});
+self.addEventListener('fetch', function (event) {
+  event.respondWith(
+    caches.match(event.request).then(function (c) { return c || fetch(event.request); })
+  );
+});
